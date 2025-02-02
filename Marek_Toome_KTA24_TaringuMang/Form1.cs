442c07bd9bd025a77e77f3ab8a06e048c3b9644c
Marek Toome <marekjah@gmail.com>
2025-02-02 08:35:28 +0200

    Add project files.

---
+++ Marek_Toome_KTA24_TaringuMang/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Marek_Toome_KTA24_TaringuMang
+{
+    public partial class MT_Form1 : Form
+    {
+        public MT_Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Marek_Toome_KTA24_TaringuMang/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Marek_Toome_KTA24_TaringuMang
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MT_Form1());
+        }
+    }
+}
