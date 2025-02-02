ee9c01d2d135ad962e7124967fa68e1cffbc91b5
Marek Toome <marekjah@gmail.com>
2025-02-02 08:47:36 +0200

    Pooled objektid lisatud

--- Marek_Toome_KTA24_TaringuMang/Form1.Designer.cs
+++ Marek_Toome_KTA24_TaringuMang/Form1.Designer.cs
@@ -28,20 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.MT_EsimeneMangija = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.MT_TeineMangija = new System.Windows.Forms.TextBox();
+            this.MT_JukuPunktid = new System.Windows.Forms.TextBox();
+            this.MT_PeetriPunktid = new System.Windows.Forms.TextBox();
+            this.MT_PunktideNaitamineJuku = new System.Windows.Forms.Label();
+            this.MT_PunktideNaitaminePeeter = new System.Windows.Forms.Label();
+            this.MT_Juku1Taring = new System.Windows.Forms.Label();
+            this.MT_Juku2Taring = new System.Windows.Forms.Label();
+            this.MT_Peeter1Taring = new System.Windows.Forms.Label();
+            this.MT_Peeter2Taring = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // MT_EsimeneMangija
+            // 
+            this.MT_EsimeneMangija.BackColor = System.Drawing.SystemColors.Control;
+            this.MT_EsimeneMangija.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MT_EsimeneMangija.Font = new System.Drawing.Font("Courier New", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.MT_EsimeneMangija.ForeColor = System.Drawing.Color.ForestGreen;
+            this.MT_EsimeneMangija.Location = new System.Drawing.Point(21, 19);
+            this.MT_EsimeneMangija.Name = "MT_EsimeneMangija";
+            this.MT_EsimeneMangija.Size = new System.Drawing.Size(271, 23);
+            this.MT_EsimeneMangija.TabIndex = 0;
+            this.MT_EsimeneMangija.Text = "Esimene mängija Juku";
+            this.MT_EsimeneMangija.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // MT_TeineMangija
+            // 
+            this.MT_TeineMangija.BackColor = System.Drawing.SystemColors.Control;
+            this.MT_TeineMangija.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MT_TeineMangija.Font = new System.Drawing.Font("Courier New", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.MT_TeineMangija.ForeColor = System.Drawing.Color.ForestGreen;
+            this.MT_TeineMangija.Location = new System.Drawing.Point(474, 19);
+            this.MT_TeineMangija.Name = "MT_TeineMangija";
+            this.MT_TeineMangija.Size = new System.Drawing.Size(271, 23);
+            this.MT_TeineMangija.TabIndex = 2;
+            this.MT_TeineMangija.Text = "Teine mängija Peeter";
+            this.MT_TeineMangija.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MT_JukuPunktid
+            // 
+            this.MT_JukuPunktid.BackColor = System.Drawing.SystemColors.Control;
+            this.MT_JukuPunktid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MT_JukuPunktid.Font = new System.Drawing.Font("Courier New", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.MT_JukuPunktid.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.MT_JukuPunktid.Location = new System.Drawing.Point(21, 119);
+            this.MT_JukuPunktid.Name = "MT_JukuPunktid";
+            this.MT_JukuPunktid.Size = new System.Drawing.Size(271, 23);
+            this.MT_JukuPunktid.TabIndex = 3;
+            this.MT_JukuPunktid.Text = "Punktid";
+            this.MT_JukuPunktid.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MT_PeetriPunktid
+            // 
+            this.MT_PeetriPunktid.BackColor = System.Drawing.SystemColors.Control;
+            this.MT_PeetriPunktid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MT_PeetriPunktid.Font = new System.Drawing.Font("Courier New", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.MT_PeetriPunktid.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.MT_PeetriPunktid.Location = new System.Drawing.Point(474, 119);
+            this.MT_PeetriPunktid.Name = "MT_PeetriPunktid";
+            this.MT_PeetriPunktid.Size = new System.Drawing.Size(271, 23);
+            this.MT_PeetriPunktid.TabIndex = 4;
+            this.MT_PeetriPunktid.Text = "Punktid";
+            this.MT_PeetriPunktid.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MT_PunktideNaitamineJuku
+            // 
+            this.MT_PunktideNaitamineJuku.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MT_PunktideNaitamineJuku.Font = new System.Drawing.Font("Courier New", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.MT_PunktideNaitamineJuku.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.MT_PunktideNaitamineJuku.Location = new System.Drawing.Point(71, 159);
+            this.MT_PunktideNaitamineJuku.Name = "MT_PunktideNaitamineJuku";
+            this.MT_PunktideNaitamineJuku.Size = new System.Drawing.Size(197, 39);
+            this.MT_PunktideNaitamineJuku.TabIndex = 5;
+            // 
+            // MT_PunktideNaitaminePeeter
+            // 
+            this.MT_PunktideNaitaminePeeter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MT_PunktideNaitaminePeeter.Font = new System.Drawing.Font("Courier New", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.MT_PunktideNaitaminePeeter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.MT_PunktideNaitaminePeeter.Location = new System.Drawing.Point(515, 159);
+            this.MT_PunktideNaitaminePeeter.Name = "MT_PunktideNaitaminePeeter";
+            this.MT_PunktideNaitaminePeeter.Size = new System.Drawing.Size(197, 39);
+            this.MT_PunktideNaitaminePeeter.TabIndex = 6;
+            // 
+            // MT_Juku1Taring
+            // 
+            this.MT_Juku1Taring.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MT_Juku1Taring.Font = new System.Drawing.Font("Courier New", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.MT_Juku1Taring.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.MT_Juku1Taring.Location = new System.Drawing.Point(71, 223);
+            this.MT_Juku1Taring.Name = "MT_Juku1Taring";
+            this.MT_Juku1Taring.Size = new System.Drawing.Size(85, 39);
+            this.MT_Juku1Taring.TabIndex = 7;
+            // 
+            // MT_Juku2Taring
+            // 
+            this.MT_Juku2Taring.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MT_Juku2Taring.Font = new System.Drawing.Font("Courier New", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.MT_Juku2Taring.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.MT_Juku2Taring.Location = new System.Drawing.Point(183, 223);
+            this.MT_Juku2Taring.Name = "MT_Juku2Taring";
+            this.MT_Juku2Taring.Size = new System.Drawing.Size(85, 39);
+            this.MT_Juku2Taring.TabIndex = 8;
+            // 
+            // MT_Peeter1Taring
+            // 
+            this.MT_Peeter1Taring.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MT_Peeter1Taring.Font = new System.Drawing.Font("Courier New", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.MT_Peeter1Taring.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.MT_Peeter1Taring.Location = new System.Drawing.Point(515, 223);
+            this.MT_Peeter1Taring.Name = "MT_Peeter1Taring";
+            this.MT_Peeter1Taring.Size = new System.Drawing.Size(85, 39);
+            this.MT_Peeter1Taring.TabIndex = 9;
+            // 
+            // MT_Peeter2Taring
+            // 
+            this.MT_Peeter2Taring.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MT_Peeter2Taring.Font = new System.Drawing.Font("Courier New", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.MT_Peeter2Taring.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.MT_Peeter2Taring.Location = new System.Drawing.Point(627, 223);
+            this.MT_Peeter2Taring.Name = "MT_Peeter2Taring";
+            this.MT_Peeter2Taring.Size = new System.Drawing.Size(85, 39);
+            this.MT_Peeter2Taring.TabIndex = 10;
             // 
             // MT_Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MT_Peeter2Taring);
+            this.Controls.Add(this.MT_Peeter1Taring);
+            this.Controls.Add(this.MT_Juku2Taring);
+            this.Controls.Add(this.MT_Juku1Taring);
+            this.Controls.Add(this.MT_PunktideNaitaminePeeter);
+            this.Controls.Add(this.MT_PunktideNaitamineJuku);
+            this.Controls.Add(this.MT_PeetriPunktid);
+            this.Controls.Add(this.MT_JukuPunktid);
+            this.Controls.Add(this.MT_TeineMangija);
+            this.Controls.Add(this.MT_EsimeneMangija);
             this.Name = "MT_Form1";
             this.Text = "Taringu Mang";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox MT_EsimeneMangija;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox MT_TeineMangija;
+        private System.Windows.Forms.TextBox MT_JukuPunktid;
+        private System.Windows.Forms.TextBox MT_PeetriPunktid;
+        private System.Windows.Forms.Label MT_PunktideNaitamineJuku;
+        private System.Windows.Forms.Label MT_PunktideNaitaminePeeter;
+        private System.Windows.Forms.Label MT_Juku1Taring;
+        private System.Windows.Forms.Label MT_Juku2Taring;
+        private System.Windows.Forms.Label MT_Peeter1Taring;
+        private System.Windows.Forms.Label MT_Peeter2Taring;
     }
 }
 
